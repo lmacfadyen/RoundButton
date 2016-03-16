6bd132940612e024497f15e9284077e97e9863ca
Lawrence F MacFadyen <larry.macfadyen@gmail.com>
2016-03-16 15:28:45 -0400

    Round Button subclass initial commit

--- RoundButton/ViewController.swift
+++ RoundButton/ViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: RoundButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +21,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonPressed(sender: AnyObject) {
+        let alert = UIAlertController(title: "Round Button", message: "You clicked the button.", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
 }
 
